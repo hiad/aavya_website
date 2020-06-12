70598fa1a52e7f20ce7b242d5d5a9d6e1768c98b
Hector Alcazar <hiad99@gmail.com>
2020-06-11 21:06:20 -0500

    home page

--- src/utils/images.js
+++ src/utils/images.js
@@ -2,11 +2,11 @@ const sources = ({ mobile, tablet, desktop }) => ([
       mobile,
       {
             ...tablet,
-            media: `(min-width: 768px)`,
+            media: `(max-width: 768px)`,
       },
       {
             ...desktop,
-            media: `(min-width: 1280px)`,
+            media: `(max-width: 1280px)`,
       },
 ]);
 
